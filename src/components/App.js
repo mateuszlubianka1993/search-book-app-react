360c4c8ff4cff07653f52dfa06bee4e1373269a5
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-04-08 14:09:42 +0200

    add onFormSubmit function

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,43 @@ class App extends React.Component {
 
     state = {
         term: '',
+        booksx: [],
+        error: false,
     }
 
     onInputChange = (e) => {
-        // console.log(e.target.value);
+        console.log(e.target.value);
         this.setState({ term: e.target.value });
     };
 
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${this.state.term}`;
+    
+      fetch(apiUrl)
+        .then(response => {
+          if(response.ok) {
+            return response;
+          }
+          throw Error('Something went wrong, try again')
+        })
+        .then(response => response.json())
+        .then(result => {
+          this.setState({
+           booksx: result.items,
+          });
+        })
+        .catch(err => {
+          this.setState({error: true})
+        })
+        console.log(this.state.booksx);
+        }
+
+    
     render () {
         return (
             <div className="app-container">
-                <SearchBar onInputChange={this.onInputChange}/>
+                <SearchBar onInputChange={this.onInputChange} onFormSubmit={this.onFormSubmit}/>
             </div>
         )
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    
+
     render () {
         return (
             <div className="search-container">
-              <form className="search-form">
+              <form className="search-form" onSubmit={this.props.onFormSubmit}>
                   <input type="text" placeholder="Search book" onChange={this.props.onInputChange}/>
                   <button type="submit">Search</button>
               </form>

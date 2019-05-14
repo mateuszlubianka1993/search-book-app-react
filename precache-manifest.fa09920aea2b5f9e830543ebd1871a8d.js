925bed3eae998427ab6fa243902b3ac0da77c96c
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-05-14 14:15:19 +0200

    Updates

---
+++ precache-manifest.fa09920aea2b5f9e830543ebd1871a8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f0d2dfc84af91d45b6218a627120ebe",
+    "url": "/search-book-app-react/static/media/antique-background-brown-924007.8f0d2dfc.jpg"
+  },
+  {
+    "revision": "51edd0c58e5e817e0691",
+    "url": "/search-book-app-react/static/js/runtime~main.16e17362.js"
+  },
+  {
+    "revision": "938f89d8dc656f8245d9",
+    "url": "/search-book-app-react/static/js/main.183b9770.chunk.js"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/search-book-app-react/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "938f89d8dc656f8245d9",
+    "url": "/search-book-app-react/static/css/main.d78e7eaf.chunk.css"
+  },
+  {
+    "revision": "e50b4f8da94ae3aec28da4cc5cd51582",
+    "url": "/search-book-app-react/index.html"
+  }
+];
